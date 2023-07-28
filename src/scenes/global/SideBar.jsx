2c4b4aeb2386ff0a6fe02320ed2486b719189571
Tarcio Diniz <tarciodiniz0@gmail.com>
2023-07-28 10:04:56 -0300

    Release 1.1 - SideBar

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,37 +1,26 @@
 import React, { useState } from 'react';
-import { ProSidebar, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
+import { Sidebar as ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import { SearchOutlined } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import { Box, Badge } from '@mui/material'; // Import Badge component
 import Avatar from '@mui/material/Avatar'; // Import Avatar component
 import colors from '../../colors';
 import mockDataConversations from "../../data/MockDataConversations";
 
 const stringAvatar = (name) => {
     return {
-        sx: {
-            bgcolor: stringToColor(name),
-        },
         children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`, // Use first letters of first and last names
     };
 };
 
-const stringToColor = (string) => {
-    let hash = 0;
-    for (let i = 0; i < string.length; i++) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    const color = Math.abs(hash).toString(16).slice(0, 6);
-    return '#' + '0'.repeat(6 - color.length) + color;
-};
-
 const Sidebar = () => {
-    const [selectedConversation, setSelectedConversation] = useState(null);
+    const [selectedConversationId, setSelectedConversationId] = useState(null);
 
-    const handleContactClick = (index) => {
-        setSelectedConversation(index);
+    const handleContactClick = (conversationId) => {
+        setSelectedConversationId(conversationId);
+        console.log(`Selected conversation ID: ${conversationId}`);
     };
 
     const renderContacts = () => {
@@ -41,10 +30,17 @@ const Sidebar = () => {
                 display="flex"
                 alignItems="center"
                 p={2}
-                onClick={() => handleContactClick(index)}
-                style={{
-                    backgroundColor: selectedConversation === index ? '#f0f0f0' : 'inherit',
+                sx={{
+                    cursor: 'pointer',
+                    backgroundColor:
+                        selectedConversationId === contact.conversationId
+                            ? colors.white2[500]
+                            : 'transparent',
+                    '&:hover': {
+                        backgroundColor: colors.white2[500],
+                    },
                 }}
+                onClick={() => handleContactClick(contact.conversationId)}
             >
                 {/* Use Avatar component for contact image or fallback to contact name */}
                 {contact.avatar ? (
@@ -52,57 +48,70 @@ const Sidebar = () => {
                 ) : (
                     <Avatar {...stringAvatar(contact.name)} />
                 )}
-                <Box sx={{marginLeft: 2}}>
+                <Box sx={{ marginLeft: 2 }}>
                     <div style={{ fontWeight: 'bold' }}>{contact.name}</div>
-                    <div>{contact["last message"]}</div>
+                    <div style={{ color: colors.white2[600] }}>
+                        {contact['last message']}
+                    </div>
                 </Box>
-                <div style={{ marginLeft: 'auto', fontSize: '12px' }}>{contact["last message date"]}</div>
+                {/* Wrap last message date and Badge in a Box */}
+                <Box sx={{ marginLeft: 'auto', fontSize: '12px', textAlign: 'right' }}>
+                    <div>{contact['last message date']}</div>
+                    {/* Add Badge component to display number of unseen messages */}
+                    {!contact.hasViewedConversation && (
+                        <Badge
+                            sx={{ marginRight: '3px' }}
+                            badgeContent={contact.unseenMessages}
+                            color="success"
+                        />
+                    )}
+                </Box>
             </Box>
         ));
     };
 
     return (
         <Box>
-            <ProSidebar>
-                <SidebarHeader>
+            <ProSidebar width={400}>
+                <Menu>
                     {/* Topo do Sidebar */}
-                    <Box
-                        p={1}
-                        sx={{
-                            display: 'flex',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <IconButton sx={{ p: 1 }}>
-                            <MenuOutlinedIcon />
-                        </IconButton>
-
-                        {/* SEARCH BAR */}
+                    <MenuItem>
                         <Box
-                            display="flex"
-                            backgroundColor={colors.white1[400]}
-                            borderRadius="40px"
-                            sx={{ marginLeft: 1 }}
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
                         >
-                            <InputBase
-                                sx={{
-                                    ml: 2,
-                                    flex: 1,
-                                    width: 200,
-                                }}
-                                placeholder="Search"
-                            />
-                            <IconButton type="button" sx={{ p: 1 }}>
-                                <SearchOutlined />
+                            <IconButton sx={{ p: 1 }}>
+                                <MenuOutlinedIcon />
                             </IconButton>
+
+                            {/* SEARCH BAR */}
+                            <Box
+                                display="flex"
+                                backgroundColor={colors.white1[400]}
+                                borderRadius="40px"
+                                sx={{ marginLeft: 1 }}
+                            >
+                                <InputBase
+                                    sx={{
+                                        ml: 2,
+                                        flex: 1,
+                                        width: 200,
+                                    }}
+                                    placeholder="Search"
+                                />
+                                <IconButton type="button" sx={{ p: 1 }}>
+                                    <SearchOutlined />
+                                </IconButton>
+                            </Box>
                         </Box>
-                    </Box>
-                </SidebarHeader>
-                <Box>
-                    <SidebarContent>
+                    </MenuItem>
+                    {/* Wrap renderContacts() in a Box with overflowY set to auto and a height of 100vh */}
+                    <Box sx={{ overflowY: 'auto', height: '92vh' }}>
                         {renderContacts()}
-                    </SidebarContent>
-                </Box>
+                    </Box>
+                </Menu>
             </ProSidebar>
         </Box>
     );
